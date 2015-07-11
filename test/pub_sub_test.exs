a56da92bd2f8a0b87fc8c946d900941fd3b72f19
simonewebdesign <hello@simonewebdesign.it>
2015-07-11 16:04:35 +0100

    Refactoring test

--- test/pub_sub_test.exs
+++ test/pub_sub_test.exs
@@ -1,41 +1,20 @@
 defmodule PubSubTest do
   use ExUnit.Case
 
-  defmodule Client do
-    
-    def start(client_name) do
-      spawn(fn -> loop(client_name) end)
-    end
-
-    def loop(name) do
-      receive do
-        message -> 
-          IO.inspect "#{name} received `#{message}`"
-          loop(name)
-      end
-    end
-
-  end
-
   test "processes can subscribe to topics" do
-    topic1 = :erlang
-    topic2 = :elixir
-
-    {:ok, pid} = PubSub.start_link()
-
-    pid1 = Client.start("Client #1")
-    pid2 = Client.start("Client #2")
-    pid3 = Client.start("Client #3")
-
+    [pid1, pid2, pid3] = spawn_multiple(3)
+    {topic1, topic2} = {:erlang, :elixir}
+    PubSub.start_link()
     PubSub.subscribe(pid1, topic1)
     PubSub.subscribe(pid2, topic1)
     PubSub.subscribe(pid3, topic2)
-    
-    subscribers_to_topic1 = PubSub.subscribers(topic1)
-    subscribers_to_topic2 = PubSub.subscribers(topic2)
 
-    assert List.first(subscribers_to_topic1) == pid2
-    assert List.first(subscribers_to_topic2) == pid3
+    assert PubSub.subscribers(topic1) == [pid2, pid1]
+    assert PubSub.subscribers(topic2) == [pid3]
+  end
+
+  def spawn_multiple(times) do
+    Enum.map(1..times, fn _ -> spawn(fn -> receive do end end) end)
   end
 
 end
