6bb39e1b3a0aff7aa33be10f8d32b9522771e74d
simonewebdesign <hello@simonewebdesign.it>
2015-07-11 16:30:30 +0100

    Update mixfile

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule PubSub.Mixfile do
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      package: package,
-     deps: deps]
+     deps: []]
   end
 
   # Configuration for the OTP application
@@ -19,21 +19,8 @@ defmodule PubSub.Mixfile do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
-  defp deps do
-    []
-  end
-
   defp package do
-    [# These are the default files included in the package
+    [contributors: ["Simone Vittori"],
      licenses: ["MIT"],
      links: %{"GitHub" => "https://github.com/simonewebdesign/elixir_pubsub"}]
   end
