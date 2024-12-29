45b97c50a07c2c1de667d4cee41614be36c32ea3
Simone <hello@simonewebdesign.it>
2024-12-29 12:22:14 +0000

    Bump version: 1.1.2 → 1.1.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PubSub.Mixfile do
 
   def project do
     [app: :pubsub,
-     version: "1.1.2",
+     version: "1.1.3",
      elixir: "~> 1.0",
      description: "Publish-Subscribe utility",
      build_embedded: Mix.env == :prod,
